ae93d59f25b63ca2659098a75f73b9b42fd9d810
岸野 <130636405+mugitya00@users.noreply.github.com>
2023-06-22 14:07:11 +0900

    Update Form1.cs

--- fukuv0622/Form1.cs
+++ fukuv0622/Form1.cs
@@ -13,7 +13,20 @@ namespace fukuv0622
 
         private void button1_Click(object sender, EventArgs e)
         {
-            data[0]=random.Next(0,101);
+            int[] data = new int[10];
+            label1.Text = "";
+            int sum = 0;
+
+
+            for(int i=0;i<10;i++)
+            {
+                data[i]= random.Next(101);
+                label1.Text += $"{data[i]},";
+                sum += data[i];
+            }
+            MessageBox.Show($"{sum / 10}");
+
+            /*data[0]=random.Next(0,101);
             data[1]=random.Next(0,101);
             data[2]=random.Next(0,101);
             data[3]=random.Next(0, 101);
@@ -26,10 +39,20 @@ namespace fukuv0622
           int  i= 0;
             for (i=0; i < 10; i++)
             {
-                label1.Text = ($"{data[0]},{data[1]},{data[2]},{data[3]},{data[4]},{data[5]},{data[6]},{data[7]},{data[8]},{data[9]}");
+               label1.Text = ($"" +
+                    $"{data[0]}," +
+                    $"{data[1]}," +
+                    $"{data[2]}," +
+                    $"{data[3]}," +
+                    $"{data[4]}," +
+                    $"{data[5]}," +
+                    $"{data[6]}," +
+                    $"{data[7]}," +
+                    $"{data[8]}," +
+                    $"{data[9]}");
                int a =(data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7] + data[8] + data[9]) / 10;
-                MessageBox.Show($"{a}");
-            }
+               MessageBox.Show($"{a}");
+            }*/
 
         }
     }
